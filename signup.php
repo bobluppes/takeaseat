0b3f579d5c6ac26591d7aebe9ea59ebbf5962873
Bob <bobluppes@live.nl>
2020-03-23 14:43:09 +0100

    Thanks for signing up message

--- signup.php
+++ signup.php
@@ -14,7 +14,7 @@ if ($link === false) {
 $sql = "INSERT INTO interests (first_name, last_name, type, email) VALUES ('" . $_POST['grid-first-name'] . "', '" . $_POST['grid-last-name'] . "', '" . $_POST['grid-type'] . "', '" . $_POST['grid-email'] . "')";
 if (mysqli_query($link, $sql)) {
     mysqli_close($link);
-    header('Location: ' . $_SERVER['HTTP_REFERER']);
+    header('Location: ' . $_SERVER['HTTP_REFERER'] . "?signup=true");
 } else {
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
     mysqli_close($link);
